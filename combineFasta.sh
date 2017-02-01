62b7f5175e27ecb97302075cfef8ce29c2e464cb
rstraver <roystraver@gmail.com>
2017-02-01 17:02:23 +0100

    Fixed awk layout/bug

--- combineFasta.sh
+++ combineFasta.sh
@@ -6,13 +6,15 @@ INFASTAS=${@:2}
 OUTFASTA=$1.fasta.gz
 IDMATCH=$1.idmatch
 
-awk -v IDMATCH="$IDMATCH" '
-FNR == 1 { FILENUM += 1; READNUM = 0 }
-!((NR) % 4 == 1)  {print}
-((NR) % 4 == 1) {
-        READID=">RD:"READNUM";IN:"FILENUM
-        print READID
-        print READID"\tFILE:"FILENAME"\t"$0>IDMATCH;
-        READNUM += 1}' \
-                $INFASTAS | \
-                gzip > $OUTFASTA
+awk \
+	-v IDMATCH="$IDMATCH" \
+	'FNR == 1 { FILENUM += 1; READNUM = 0 }
+	!((NR) % 4 == 1) {
+		print}
+	((NR) % 4 == 1)	{
+		READID=">RD:"READNUM";IN:"FILENUM
+	        print READID
+	        print READID"\tFILE:"FILENAME"\t"$0>IDMATCH;
+	        READNUM += 1}' \
+	                $INFASTAS | \
+	                gzip > $OUTFASTA
