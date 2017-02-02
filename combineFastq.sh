8dfee5b8af553e0c9817e3ac49648c611f8936fe
rstraver <roystraver@gmail.com>
2017-02-02 15:34:29 +0100

    Fixed fasta filespec error

--- combineFastq.sh
+++ combineFastq.sh
@@ -16,14 +16,14 @@ awk \
 	(FNR == 1) {
 		++FILENUM;
 		READNUM = 0 }
-	((NR % 4096) == 0) {
+	((NR % 16384) == 0) {
 		++C;
 		CUROUT = OUTFASTA"_"C".fa" }
 	((NR) % 4 == 1)	{
-		READID = "@RD:"READNUM";IN:"FILENUM;
-		print READID > CUROUT;
-	        print READID"\t"FILENAME"\t"$0 | "gzip >" IDMATCH".gz";
-		print READID | "gzip >" OUTFASTA".fastq.gz"
+		READID = "RD:"READNUM";IN:"FILENUM;
+		print ">"READID > CUROUT;
+	        print ">"READID"\t"FILENAME"\t"$0 | "gzip >" IDMATCH".gz";
+		print "@"READID | "gzip >" OUTFASTA".fastq.gz"
 	        ++READNUM}
 	((NR) % 4 == 2) {
 		print $0 > CUROUT }
