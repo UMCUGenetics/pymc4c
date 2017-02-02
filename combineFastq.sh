c7bf20b72db7f327871cc5d72b3293afdd000272
rstraver <roystraver@gmail.com>
2017-02-02 14:16:22 +0100

    Split fasta approach added to awk

--- combineFastq.sh
+++ combineFastq.sh
@@ -3,18 +3,32 @@
 #OUTFASTQ=test.FASTQ.gz
 
 INFASTQS=${@:2}
-OUTFASTQ=$1.fastq.gz
+OUTFASTA=$1
 IDMATCH=$1.idmatch
 
 awk \
 	-v IDMATCH="$IDMATCH" \
-	'FNR == 1 { FILENUM += 1; READNUM = 0 }
-	!((NR) % 4 == 1) {
-		print}
+	-v OUTFASTA="$OUTFASTA" \
+	'
+	BEGIN{
+		C=0;
+	CUROUT = OUTFASTA"_"C".fa"}
+	(FNR == 1) {
+		++FILENUM;
+		READNUM = 0 }
+	((NR % 4096) == 0) {
+		++C;
+		CUROUT = OUTFASTA"_"C".fa" }
 	((NR) % 4 == 1)	{
-		READID="@RD:"READNUM";IN:"FILENUM
-	        print READID
-	        print READID"\tFILE:"FILENAME"\t"$0>IDMATCH;
-	        READNUM += 1}' \
-	                $INFASTQS | \
-	                gzip > $OUTFASTQ
+		READID = "@RD:"READNUM";IN:"FILENUM;
+		print READID > CUROUT;
+	        print READID"\t"FILENAME"\t"$0 | "gzip >" IDMATCH".gz";
+		print READID | "gzip >" OUTFASTA".fastq.gz"
+	        ++READNUM}
+	((NR) % 4 == 2) {
+		print $0 > CUROUT }
+	!((NR) % 4 == 1) {
+		print $0 | "gzip >" OUTFASTA".fastq.gz"}
+	END {}' \
+	                $INFASTQS #> \
+				#$OUTFASTA.fastq.gz
