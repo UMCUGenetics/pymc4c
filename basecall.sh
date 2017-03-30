7d00891c906ceab34096795dd9f490a59976377d
aminbys <aminbys@gmail.com>
2017-03-30 09:05:38 +0200

    Added base call script

--- basecall.sh
+++ basecall.sh
@@ -1,11 +1,11 @@
-#$ -l h_rt=04:00:00
-#$ -l h_vmem=24G
-#$ -pe threaded 12
+#$ -l h_rt=05:00:00
+#$ -l h_vmem=10G
+#$ -pe threaded 6
 
 BASECALL=/hpc/cog_bioinf/kloosterman/users/wkloosterman/tools/albacore/albacore_venv/bin/read_fast5_basecaller.py
 FILE_INPUT=$1
-FILE_OUT=${FILE_INPUT}_basecalled
-F5_CFG=FLO-MIN106_LSK108_linear.cfg
+FILE_OUT=$2
+F5_CFG=$3
 
 THIS_ID=$((SGE_TASK_ID-1))
 
@@ -15,6 +15,6 @@ module load python/3.4.3
 
 $BASECALL \
         --input ${FILE_INPUT}/${THIS_ID}/ \
-        --worker_threads 12 \
+        --worker_threads 6 \
         --save_path ${FILE_OUT}/${THIS_ID}/ \
         --config ${F5_CFG}
